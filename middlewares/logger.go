ee49cd61983e15279516af03e0b119bb1c31b454
Bernardo Forcillo <bernardoforcillo@outlook.com>
2025-06-29 15:01:34 +0200

    refactor: enhance logger middleware configuration options

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -1,26 +1,52 @@
 package middlewares
 
 import (
-	"fmt"
+	"log"
 	"time"
 
 	"github.com/buildwithgo/amaro"
 )
 
-func Logger() amaro.Middleware {
+type LoggerOption func(*loggerConfig)
+type LoggerPrintFunc func(logger *log.Logger, duration time.Duration, c *amaro.Context)
+
+type loggerConfig struct {
+	logger    *log.Logger
+	printFunc LoggerPrintFunc
+}
+
+func WithLogger(logger *log.Logger) LoggerOption {
+	return func(cfg *loggerConfig) {
+		cfg.logger = logger
+	}
+}
+
+func WithLoggerLogFunc(logFunc LoggerPrintFunc) LoggerOption {
+	return func(cfg *loggerConfig) {
+		cfg.printFunc = logFunc
+	}
+}
+
+func Logger(opts ...LoggerOption) amaro.Middleware {
+	cfg := &loggerConfig{
+		logger: log.Default(),
+		printFunc: func(logger *log.Logger, duration time.Duration, c *amaro.Context) {
+			logger.Printf("%s %s - %s  \n",
+				c.Request.Method,
+				c.Request.URL.Path,
+				duration,
+			)
+		},
+	}
+	for _, opt := range opts {
+		opt(cfg)
+	}
 	return func(next amaro.Handler) amaro.Handler {
 		return func(c *amaro.Context) error {
 			start := time.Now()
 			err := next(c)
 			duration := time.Since(start)
-
-			fmt.Printf("[%s] %s %s - %s %v\n",
-				time.Now().Format("2006-01-02 15:04:05"),
-				c.Request.Method,
-				c.Request.URL.Path,
-				duration,
-				err,
-			)
+			cfg.printFunc(cfg.logger, duration, c)
 			return err
 		}
 	}
